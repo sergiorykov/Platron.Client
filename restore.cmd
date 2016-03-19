2d48239c8ec64904a9b9f625226bd8dfa3352915
Sergio Rykov <SergioR@bk.ru>
2016-03-19 15:25:02 +0300

    included Tools/nuget.exe to enable unattended nuget restore

--- restore.cmd
+++ restore.cmd
@@ -2,7 +2,8 @@
 
 rem Partially based on ideas from https://github.com/loresoft/KickStart
 
+Tools\nuget.exe install Tools\packages.config -OutputDirectory Tools -ExcludeVersion -NonInteractive
+
 md Source\packages
 Tools\nuget.exe restore Source\Platron.sln -PackagesDirectory Source\packages
 
-Tools\nuget.exe install Tools\packages.config -OutputDirectory Tools -ExcludeVersion -NonInteractive
